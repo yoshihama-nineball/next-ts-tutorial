0af75e5b13cdab74b87e6e83aa5a02f15b541da1
ezu0 <burimofumofu@gmail.com>
2025-03-29 20:40:42 +0900

    setting: global.cssの適用

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 // server component
 import React from 'react';
+import '../styles/global.css';
 import ClientThemeWrapper from './components/layouts/ClientThemeWrapper';
 import Footer from './components/layouts/Footer/Footer';
 import Header from './components/layouts/Header/Header';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>
         <ClientThemeWrapper>
           <Header />
